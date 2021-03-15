2791065ee51fea341fdb0fd9d0a86251f602a39a
dyuganov-n <dyuganov.n@gmail.com>
2021-03-16 01:30:27 +0700

    fix

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -11,9 +11,10 @@ void openAndCloseFile(const char* fileName){
 		perror("Can't open file");
 		return;
 	}
+	printf("File opened successfully \n");
 
 	const int resultFclose = fclose(file);
-  	const int FCLOSE_SUCCESS = 0;
+  const int FCLOSE_SUCCESS = 0;
 	if (resultFclose != FCLOSE_SUCCESS){
 		perror("Fclose error");
 		return;
@@ -24,22 +25,28 @@ int main(int argc, char **argv){
 	printf("Real user ID %ld \n", getuid());
 	printf("Effective user ID %ld \n", geteuid());
 
-  	const int ARGC_CORRECT = 2;
+	const int ARGC_CORRECT = 2;
 	if (argc != ARGC_CORRECT) {
 		perror("Need file");
 		return EXIT_FAILURE;
 	}
 
+	printf("Trying to open the file: \n");
 	const short fileNameArgcIdx = 1;
 	openAndCloseFile(argv[fileNameArgcIdx]);
 
+	const int SETUID_ERR = -1;
 	uid_t uid = getuid();
-	setuid(uid);
+	int setuidResult = setuid(uid);
+	if(setuidResult == SETUID_ERR){
+		perror("setuid error. Wrong id");
+	}
 
 	printf("After using setuid \n");
 	printf("Real user ID %ld \n", getuid());
 	printf("Effective user ID %ld \n", geteuid());
 
+	printf("Trying to open the file: \n");
 	openAndCloseFile(argv[fileNameArgcIdx]);
 
 	return EXIT_SUCCESS;
